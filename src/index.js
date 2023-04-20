685a630bff233958bb0f41f5f6b6241500c37d3c
Shrish Dhakal <shrishd242@gmail.com>
2023-04-20 16:47:14 +0545

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const {Client , IntentsBitField, Message} = require('discord.js');
 
-const fetch = import('node-fetch').then((module) => module.default);
+const { Configuration, OpenAIApi } = require('openai');
+
+// const fetch = import('node-fetch').then((module) => module.default);
 
 const client = new Client({
     intents: [
@@ -15,35 +17,23 @@ const client = new Client({
 client.on('ready' , (c) => {
     console.log(`${c.user.tag} is online.`);
 });
-client.on('messageCreate', (message) => {
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+client.on('messageCreate', async (message) => {
     console.log('Message received: ' + message.content);
     if (message.content === 'hello') {
       message.reply('Hey!');
     }
-  });
-  client.on('messageCreate', (message) => {
-    console.log('Message received: ' + message.content);
-    if (message.content === 'muji') {
-      message.reply('Watch your language');
-    }
-    if (message.content === 'lado') {
-      message.reply('Watch your language');
-    }
-    
-  });
-  client.on('messageCreate', (message) => {
-    console.log('Message received: ' + message.content);
   if (message.content === 'nice') {
     message.react('😀')
     message.reply('nice thing')
   }
-  if (message.content === 'sujan') {
-    message.react('😚')
-    message.reply('gay ho lado')
-  }
-});
-client.on('messageCreate', (message) => {
-  console.log('Message received: ' + message.content);
   if(message.content === 'flip'){ 
     message.reply('Machina is flipping the coin.....')
 var flip = Math.floor(Math.random()*2) + 1;
@@ -57,26 +47,51 @@ else
   message.reply('It is tail');
 }
   }
+  if (message.author.bot) return;
+  if (message.channel.id !== process.env.CHANNEL_ID) return;
+  if (message.content.startsWith('!')) return;
+
+  const conversationLog = [
+    { role: 'system', content: 'You are a friendly bot' },
+    { role: 'user', content: message.content },
+  ];
+
+  await message.channel.sendTyping();
+
+  const result = await openai.createChatCompletion({
+    model: 'text-davinci-002',
+    prompt: '',
+    maxTokens: 150,
+    temperature: 0.5,
+    n: 1,
+    stop: ['\n'],
+    presencePenalty: 0.6,
+    frequencyPenalty: 0.6,
+    context: conversationLog,
+  });
+
+  message.reply(result.data.choices[0].text);
 });
-async function getmsg(message){ 
-client.on('messageCreate', async (message) => {
-  console.log('Message received: ' + message.content);
+// async function getmsg(message){ 
+// client.on('messageCreate', async (message) => {
+//   console.log('Message received: ' + message.content);
 
-  if (message.content === '!gif'){ 
-    message.reply('gif!'); 
-    let url = `https://tenor.googleapis.com/v2/search?q=excited&key=${process.env.TENOR}&client_key=my_test_app&limit=8`
-    let reponse = await fetch(url);
-    let json = await reponse.json();
-    console.log(json);
+//   if (message.content === '!gif'){ 
+//     message.reply('gif!'); 
+//     let url = `https://tenor.googleapis.com/v2/search?q=excited&key=${process.env.TENOR}&client_key=my_test_app&limit=8`
+//     let reponse = await fetch(url);
+//     let json = await reponse.json();
+//     console.log(json);
   
   
-  }
-});
+//   }
+// });
 
+// client.login(process.env.TOKEN);
+// }
+
+  
 client.login(process.env.TOKEN);
-}
 
-  
-client.login(process.env.TOKEN);
+// getmsg()
 
-getmsg()
